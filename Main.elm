ebdfb9289b19537d031e7f8b7ef29d9bab9a4fe4
Rohith Daka <rohith.daka@gmail.com>
2016-05-08 12:27:15 -0700

    Indian Number formating works

--- Main.elm
+++ Main.elm
@@ -1,42 +1,72 @@
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 import Basics exposing (..)
 import String exposing (..)
+import StartApp.Simple as S exposing(..)
 
 
 
 type alias Model = 
-    { savingsAmount: Float
-    , priceOfBiryani: Float
-    , numberOfBiryanis: Float
+    { savingsAmount: String
+    , priceOfBiryani: String
     }
+
 
 initialModel = 
-    { savingsAmount = 10000000000000
-    , priceOfBiryani = 100
+    { savingsAmount = 1000000000 |> toString |> indianNumberFormat
+    , priceOfBiryani = 1000 |> toString |> indianNumberFormat
     }
 
+type Action 
+    = UpdateSavings String
+    | UpdatePrice String
+
+update: Action -> Model -> Model
+update action model =
+    case action of 
+        UpdateSavings n -> {model | savingsAmount = (n |> indianNumberFormat)}
+        UpdatePrice n -> {model | priceOfBiryani = (n |> indianNumberFormat)}
+
+
 indianNumberFormat: String -> String
-indianNumberFormat value =
+indianNumberFormat n =
     let 
-        l = String.length value
+        cleansedNumericString = concat (split "," n)
+        l = String.length cleansedNumericString
     in 
         if l < 4 then 
-            value
+            cleansedNumericString
         else if (l % 2 == 0) then
-            (left 1 value) ++ "," ++ indianNumberFormat (dropLeft 1 value)
+            (left 1 cleansedNumericString) ++ "," ++ indianNumberFormat (dropLeft 1 cleansedNumericString)
         else
-            (left 2 value) ++ "," ++ indianNumberFormat (dropLeft 2 value)
+            (left 2 cleansedNumericString) ++ "," ++ indianNumberFormat (dropLeft 2 cleansedNumericString)
 
 
-view model =
+view address model =
+    let savingsAmountString = model.savingsAmount
+        savingsAmountLength = String.length savingsAmountString
+    in
     div []
-        [ h1 [] [text "Biryani Economics"]
-        , p [] [ text ("Suppose you have ₹ " ++ (model.savingsAmount |> toString |> indianNumberFormat) ++ " and the price of biryani is ₹ " ++ (model.priceOfBiryani |> toString))]
+        [ h1 
+            [] 
+            [text "Biryani Economics"]
+        , p 
+            [] 
+            [ text "Suppose you have ₹ "
+            , input 
+                [ type' "text"
+                , value savingsAmountString
+                , size savingsAmountLength
+                , on "input" targetValue (Signal.message address << UpdateSavings)
+                ]
+                []
+            , text (" and the price of biryani is ₹ " 
+                    ++ (model.priceOfBiryani |> indianNumberFormat))]
         ]
 
 
 
 
 main =
-    view initialModel
+    S.start {model = initialModel, update =update, view =view}
