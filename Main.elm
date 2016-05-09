f03c0fcd2863f4472e88cde3294dfcb080deb81f
Rohith Daka <rohith.daka@gmail.com>
2016-05-08 19:39:15 -0700

    Working prototype is ready. Todo- beautify

--- Main.elm
+++ Main.elm
@@ -12,6 +12,7 @@ type alias Model =
     { savings: String
     , price: String
     , interest: Float
+    , inflation: Float
     , numberOfYears: Float
     }
 
@@ -21,6 +22,7 @@ initialModel =
     , price = 250 |> toString |> indianNumberFormat
     , interest = 5
     , numberOfYears = 1
+    , inflation = 3
     }
 
 type Action 
@@ -28,6 +30,7 @@ type Action
     | UpdatePrice String
     | UpdateInterest String
     | UpdateYears String
+    | UpdateInflation String
 
 update: Action -> Model -> Model
 update action model =
@@ -36,6 +39,7 @@ update action model =
         UpdatePrice n -> {model | price = (n |> indianNumberFormat)}
         UpdateInterest n -> {model | interest = (n |> String.toFloat|> Result.toMaybe |> Maybe.withDefault 0)}
         UpdateYears n -> {model | numberOfYears = (n |> String.toFloat|> Result.toMaybe |> Maybe.withDefault 0)}
+        UpdateInflation n -> {model | inflation = (n |> String.toFloat|> Result.toMaybe |> Maybe.withDefault 0)}
 
 
 cleanseToNumeric: String -> String
@@ -61,8 +65,8 @@ rinf numeric =
             (left 2 numeric) ++ "," ++ rinf (dropLeft 2 numeric)
 
 
-numberOfBiryanis: Model -> Int  
-numberOfBiryanis model =
+initialBiryanis: Model -> Int  
+initialBiryanis model =
     let s = model.savings |> cleanseToNumeric |> String.toInt|> Result.toMaybe |> Maybe.withDefault 0
         p = model.price |> cleanseToNumeric |> String.toInt|> Result.toMaybe |> Maybe.withDefault 100
     in 
@@ -72,6 +76,37 @@ finalSavings: Model -> Int
 finalSavings model =
     let s = model.savings |> cleanseToNumeric |> String.toFloat|> Result.toMaybe |> Maybe.withDefault 0 
     in s*(1 + model.interest / 1200 )^(12*model.numberOfYears) |> round
+
+finalPrice: Model -> Int
+finalPrice model =
+    let p = model.price |> cleanseToNumeric |> String.toFloat|> Result.toMaybe |> Maybe.withDefault 0 
+    in p* (1 + model.inflation / 1200 )^(12*model.numberOfYears) |> round
+
+finalBiryanis: Model -> Int 
+finalBiryanis model =
+    (model |> finalSavings)//(model |> finalPrice)
+
+
+concludingRemark: Model -> String
+concludingRemark model =
+    let f = (model |> finalBiryanis )
+        i = (model |> initialBiryanis)
+    in 
+        if f < i then
+            "It is like you lost your money. You should be more careful by keeping your money some place where you get more interest or by demanding your elected representatives to reduce inflation"
+        else if f == i then
+            "You managed to protect your money's value. Not bad. You kept up with the inflation."
+        else if f > i && f < ( (Basics.toFloat i) * (1 + 7/1200)^(12*model.numberOfYears) |> round ) then
+            "Good Job! You became richer by beating the inflation"
+        else 
+            "Excellent!! You either worked really hard or have resorted to crony rent seeking mechanism through politicians "
+
+yearString: Model -> String
+yearString model =
+    if model.numberOfYears == 1 then
+        " year"
+    else 
+        " years"
 
 view address model =
     div []
@@ -93,10 +128,8 @@ view address model =
                 ]
                 []
             , text " in your savings account." 
-            ]
-        , p 
-            []
-            [ text "Biryani price is ₹ " 
+            , br [] []
+            , text "Biryani price is ₹ " 
             , input 
                 [ type' "text"
                 , value model.price
@@ -104,11 +137,11 @@ view address model =
                 , on "input" targetValue (Signal.message address << UpdatePrice)
                 ]
                 []
-            , text (". So you can buy " ++ (model |> numberOfBiryanis |> toString |> indianNumberFormat) ++ " biryanis right now.")
+            , text (". So you can buy " ++ (model |> initialBiryanis |> toString |> indianNumberFormat) ++ " biryanis right now.")
             ]
         , p
             []
-            [ text "Now let us consider the interest rate of your savings account. "
+            [ text "Let us consider the interest rate of your savings account. "
             , input 
                 [ type' "range"
                 , value (toString model.interest)
@@ -119,20 +152,53 @@ view address model =
                 ]
                 []
             , text ((toString model.interest) ++ "%")
-            ]
-        , p 
-            []
-            [ text "This means, after " 
+            , br [] []
+            , text "This means, after " 
             , input 
                 [ type' "number"
                 , value (toString model.numberOfYears) 
+                , size 2
                 , HA.max "999"
                 , HA.min "0"
                 , HA.step "1"
                 , on "change" targetValue (Signal.message address << UpdateYears)
                 ]
                 []
-            , text (" year, you would have ₹ " ++ (model |> finalSavings |> toString |> indianNumberFormat) )
+            , text (yearString model)
+            , text (", you would have ₹ " ++ (model |> finalSavings |> toString |> indianNumberFormat) )
+            ]
+        , p 
+            []
+            [ text "However, if the inflation of the currency is" 
+            , input 
+                [ type' "range"
+                , value (toString model.inflation)
+                , HA.max "10"
+                , HA.min "0"
+                , HA.step "0.01"
+                , on "input" targetValue (Signal.message address << UpdateInflation)
+                ]
+                []
+            , text ((toString model.inflation) ++ "%")
+            , text (", it increases the price of biryani to ₹ " ++ (model |> finalPrice |> toString |> indianNumberFormat) ++ " after ")
+            ,  input 
+                [ type' "number"
+                , value (toString model.numberOfYears) 
+                , size 2
+                , HA.max "999"
+                , HA.min "0"
+                , HA.step "1"
+                , on "change" targetValue (Signal.message address << UpdateYears)
+                ]
+                []
+            , text (yearString model)
+            ]
+        , p 
+            []
+            [ text "So you can buy "
+            , text (model |> finalBiryanis |> toString |> indianNumberFormat )
+            , text " biryanis. "
+            , text (model |> concludingRemark)
             ]
         ]
 
